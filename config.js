84be1b4dfdcab9cddcf22ad7790161ae9dc1ce86
arnold956 <arnoldrodgers14@gmail.com>
2026-08-26 18:46:47 +0300

    Add centralized GOON FX configuration

---
+++ config.js
@@ -0,0 +1,7 @@
+window.GOONFX_CONFIG = {
+  DERIV_CLIENT_ID: 'YOUR_DERIV_OAUTH_CLIENT_ID',
+  DERIV_REDIRECT_URI: 'https://goonfx.com/callback.html',
+  DERIV_SCOPE: 'trade',
+  BACKEND_URL: 'https://api.goonfx.com',
+  GOOGLE_CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID'
+};
