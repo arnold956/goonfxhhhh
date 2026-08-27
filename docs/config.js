1275c9091ef3b792bd0c6702bc077748ba97d49d
arnold956 <arnoldrodgers14@gmail.com>
2026-08-27 06:53:20 +0300

    Configure GOON FX for Deriv OAuth backend flow

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,9 @@
 window.GOONFX_CONFIG={
+  DERIV_CLIENT_ID:'34b2ctEChXoL5t579q8pB',
   DERIV_APP_ID:'34b2ctEChXoL5t579q8pB',
-  DERIV_SCOPE:'trade',
+  DERIV_SCOPE:'trade account_manage application_read payment',
   DERIV_REST_BASE:'https://api.derivws.com',
-  DERIV_PUBLIC_WS:'wss://api.derivws.com/trading/v1/options/ws/public',
-  BACKEND_URL:'',
+  DERIV_REDIRECT_URI:'https://goonfx.com/callback.html',
+  BACKEND_URL:'https://api.goonfx.com',
   GOOGLE_CLIENT_ID:'YOUR_GOOGLE_CLIENT_ID'
 };
