e27b476e35479e644d70b4f43c39448784de6b02
arnold956 <arnoldrodgers14@gmail.com>
2026-08-27 23:04:13 +0300

    Align Deriv OAuth client and root redirect with registered production configuration

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.GOONFX_CONFIG = {
-  DERIV_CLIENT_ID: '34b2ctEChXoL5t579q8pB',
-  DERIV_REDIRECT_URI: 'https://goonfx.com/callback.html',
+  DERIV_CLIENT_ID: '348AuAfk8ZpsbSW8Whqc3',
+  DERIV_REDIRECT_URI: 'https://goonfx.com/',
   DERIV_SCOPE: 'trade',
   BACKEND_URL: 'https://api.goonfx.com',
   GOOGLE_CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID'
