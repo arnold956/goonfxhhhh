15ea5dc3eace374b28903dfd24a2a36d48c50f57
arnold956 <arnoldrodgers14@gmail.com>
2026-08-28 19:07:28 +0300

    fix: align frontend OAuth client and callback with production backend

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,6 @@
 window.GOONFX_CONFIG = Object.freeze({
+  DERIV_CLIENT_ID: '34b2ctEChXoL5t579q8pB',
+  DERIV_SCOPE: 'trade',
+  DERIV_REDIRECT_URI: 'https://goonfx.com/callback.html',
   BACKEND_URL: 'https://api.goonfx.com'
 });
