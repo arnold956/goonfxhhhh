23981b89901212bfecc69492ab50134de257340d
arnold956 <arnoldrodgers14@gmail.com>
2026-08-28 19:07:22 +0300

    fix: use production Deriv OAuth configuration from backend

--- docs/config.js
+++ docs/config.js
@@ -1,6 +1,3 @@
 window.GOONFX_CONFIG = Object.freeze({
-  DERIV_CLIENT_ID: '348AuAfk8ZpsbSW8Whqc3',
-  DERIV_SCOPE: 'trade',
-  DERIV_REDIRECT_URI: 'https://goonfx.com/',
   BACKEND_URL: 'https://api.goonfx.com'
 });
