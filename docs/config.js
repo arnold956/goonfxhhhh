c7ed7286209a6ac72beee5cc955074e6cc961fde
arnold956 <arnoldrodgers14@gmail.com>
2026-08-28 16:22:04 +0300

    fix: remove Google login and use Deriv OAuth only

--- docs/config.js
+++ docs/config.js
@@ -1,8 +1,6 @@
 window.GOONFX_CONFIG = Object.freeze({
   DERIV_CLIENT_ID: '348AuAfk8ZpsbSW8Whqc3',
-  DERIV_APP_ID: '',
   DERIV_SCOPE: 'trade',
   DERIV_REDIRECT_URI: 'https://goonfx.com/',
-  BACKEND_URL: 'https://api.goonfx.com',
-  GOOGLE_CLIENT_ID: 'YOUR_GOOGLE_CLIENT_ID'
+  BACKEND_URL: 'https://api.goonfx.com'
 });
